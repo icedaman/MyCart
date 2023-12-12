54a9fe8ab60c9f5c7540f36a903a115a065a8707
Joao Alegria <joao.alegria@openbrix.co.uk>
2023-12-12 09:01:24 +0000

    SuperQuality section created

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,9 @@ const Button = ({ label, iconURL}) => {
       px-7 py-4 border font-montserrat leading-none gap-2 bg-coral-red border-coral-red rounded-full text-white'
     >
       {label}
-      <img src={iconURL} alt="arrow-right-icon" className="ml-2 rounded-full w-5 h-5" />
+      {iconURL &&
+        <img src={iconURL} alt="arrow-right-icon" className="ml-2 rounded-full w-5 h-5" />
+      }
     </button>
   )
 }
