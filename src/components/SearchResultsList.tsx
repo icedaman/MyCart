014dae0ce6238fe1a9c7bd93b83cb94603a881af
Joao Alegria <joao.alegria@openbrix.co.uk>
2023-12-29 03:41:02 +0000

    height bug fix

--- src/components/SearchResultsList.tsx
+++ src/components/SearchResultsList.tsx
@@ -15,7 +15,7 @@ export const SearchResultsList = ({ results, setResults, setInput }) => {
           <>
             <NavLink to={`/products/${item.category}/${item.id}`} onClick={handleClick}>
               <p key={item.id} className="p-4 border-b-2 hover:bg-coral-red hover:text-white hover:text-2xl text-xl font-montserrat font-medium">
-                  <img src={item.imgURL} className="w-[120px] inline-block mr-16" alt="item pic" />{item.name}
+                  <img src={item.imgURL} className="w-[120px] max-h-[120px] inline-block mr-16" alt="item pic" />{item.name}
                 </p> 
             </NavLink>
           </>
