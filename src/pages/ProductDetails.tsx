4ac21687b13768d7700aee640650ccb41e72939a
Joao Alegria <joao.alegria@openbrix.co.uk>
2023-12-31 02:10:15 +0000

    color changes to product cards

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,12 +1,12 @@
 
 const ProductDetails = ({ name, price, imgURL }) => {
   return (
-    <div className="max-container p- my-2 transition duration-500 ease-in-out shadow-2xl  bg-zinc-400 cursor-pointer rounded-2xl transform hover:scale-110 hover:shadow-[0_0_50px_orange]">    
-      <h1 className='py-2 px-2 text-2xl  text-amber-300 text-center  capitalize font-bold [text-shadow:1px_1px_4px_#000]'>{name}</h1>   
+    <div className="max-container p- my-2 transition duration-500 ease-in-out shadow-2xl  bg-white cursor-pointer rounded-2xl transform hover:scale-110 hover:shadow-[0_0_50px_orange]">    
+      <h1 className='py-2 px-2 text-3xl  text-amber-300 text-center  capitalize font-bold [text-shadow:1px_1px_4px_#000]'>{name}</h1>   
       <div className="p-8 rounded ">
           <img src={imgURL} width={250} className="m-auto h-28 object-contain hover:scale-125 transition-transform ease-in-out duration-500" />
       </div>
-      <h4 className='text-2xl py-2 px-2 font-semibold capitalize text-clip overflow-hidden text-center text-white'>{price}</h4>
+      <h4 className='text-2xl py-2 px-2 font-semibold capitalize text-clip overflow-hidden text-center text-coral-red'>{price}</h4>
     </div>
   )
 }
