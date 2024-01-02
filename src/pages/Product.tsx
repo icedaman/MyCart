773a17ca8cef1bebea0e4a4f945b34829e9ec96e
Joao Alegria <joao.alegria@openbrix.co.uk>
2024-01-02 00:15:36 +0000

    animations to products page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,6 +1,7 @@
 import { NavLink, Outlet, useParams, useSearchParams} from 'react-router-dom'
 import { shoeList, pantsList, jacketList, trackSuiteList } from '../constants'
 import Button from '../components/Button'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Product = () => {
   const queryId = useParams()
@@ -9,20 +10,22 @@ const Product = () => {
 
   return (
     <div className='bg-hero'>
-      <div className="max-container py-20">
-        {allProductsList.filter(item => item.id === Number(queryId.id) && item.category === queryId.category).map(filteredItem => (
-          <div key={filteredItem.id} className='w-2/4 m-auto py-4 bg-white rounded-xl'>
-            <div className='flex flex-col'>
-              <p className='my-4 text-4xl  text-amber-300 text-center  capitalize font-bold [text-shadow:1px_1px_4px_#000]'>{filteredItem.name}</p>
-              <img src={filteredItem.imgURL} alt="item" className='w-80 my-10 m-auto hover:scale-125 transition-transform duration-500 ease-in-out max-h-[500px]' />
-              <p className='my-4 text-3xl  text-amber-300 text-center  capitalize font-bold [text-shadow:1px_1px_4px_#000]'>{filteredItem.price}</p>
-            </div>
-            <div className='m-auto my-4 w-2/4'>
-              <Button label={'Add to Cart'} iconURL={undefined} backgroundColor={undefined} borderColor={undefined} textColor={undefined} fullWidth={true} />
-            </div>
-          </div>
-        ))}
-      </div>
+        <motion.div layout className="max-container py-20">
+          <AnimatePresence>
+            {allProductsList.filter(item => item.id === Number(queryId.id) && item.category === queryId.category).map(filteredItem => (
+              <div key={filteredItem.id} className='w-2/4 m-auto py-4 bg-white rounded-xl'>
+                <div className='flex flex-col'>
+                  <p className='my-4 text-4xl  text-amber-300 text-center  capitalize font-bold [text-shadow:1px_1px_4px_#000]'>{filteredItem.name}</p>
+                  <img src={filteredItem.imgURL} alt="item" className='w-80 my-10 m-auto hover:scale-125 transition-transform duration-500 ease-in-out max-h-[500px]' />
+                  <p className='my-4 text-3xl  text-amber-300 text-center  capitalize font-bold [text-shadow:1px_1px_4px_#000]'>{filteredItem.price}</p>
+                </div>
+                <div className='m-auto my-4 w-2/4'>
+                  <Button label={'Add to Cart'} iconURL={undefined} backgroundColor={undefined} borderColor={undefined} textColor={undefined} fullWidth={true} />
+                </div>
+              </div>
+            ))}
+          </AnimatePresence>
+        </motion.div>
     </div>
   )
 }
